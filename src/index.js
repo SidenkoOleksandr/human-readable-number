fc6d27ec0409d470a368d50ea023a27831f62a1c
SidenkoOleksandr <sidoj85@gmail.com>
2022-02-21 00:51:10 +0200

    feat: add human-reable-number task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function toReadable(number) {
-    if (number === 0) result = "zero"; else {
-        
-    }
+    if (number === 0) result = "zero";
+    else if (number === 1) result = "one";
     return result;
 };
